cc2d1fb86503fa20d5254ab571bbfd1ed649a4fa
Souradip2022 <22052939@kiit.ac.in>
2024-05-04 12:13:32 +0530

    Added video model Schema

---
+++ src/models/users.model.js
@@ -0,0 +1,53 @@
+import mongoose, {Schema, model} from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true //Should be used only with specific keys because it decreases performance
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+      }
+    ],
+    password: {
+      type: String,
+      required: [true, 'Password is required']
+    },
+    refreshToken: {
+      type: String
+    }
+
+  },
+  {
+    timestamps: true
+  }
+)
+
+export const User = model("User", userSchema);
